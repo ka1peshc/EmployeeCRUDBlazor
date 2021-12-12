fef2608642600c83ffcccd40aa08d99224fdf49c
ka1peshc <kalpesh111997@gmail.com>
2021-12-12 21:55:11 +0530

    [Kalpesh]Add Project File structure

---
+++ EmployeeModel/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeModel
+{
+    public class Employee
+    {
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string ProfileImage { get; set; }
+        public string Gender { get; set; }
+        public string Department { get; set; }
+        public int Salary { get; set; }
+        public DateTime StartDate { get; set; }
+        public string Notes { get; set; }
+    }
+}
